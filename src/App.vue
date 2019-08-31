85f429aabd8c7b84463798362103803ca53aebc4
Alexander Velichko <alvi@mail.ua>
2019-08-31 22:35:15 +0300

    added team page

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,22 @@
   </div>
 </template>
 
-<style lang="scss">
-@import "@/global-styles/color.scss";
-@import "@/global-styles/typography.scss";
+<script>
+import "animate.css";
 
+export default {
+  name: "App",
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+}
 body {
   background-color: $dark-blue;
 }
@@ -16,7 +28,6 @@ body {
   font-family: $system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: $white;
 }
 #nav {
@@ -35,5 +46,98 @@ h1 {
 
 p {
   @include large-text-bold($purple);
+}
+
+h4 {
+  margin: 0;
+  line-height: 34px;
+  font-size: 24px;
+  text-align: center;
+  color: #ffffff;
+}
+
+input {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 60px;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  padding-left: 20px;
+  margin-top: 20px;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+button {
+  background: #56ccf2;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  border: none;
+  height: 60px;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  margin-top: 20px;
+  margin-bottom: 40px;
+}
+
+a {
+  line-height: 25px;
+  font-size: 16px;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.3);
+  text-decoration: none;
+}
+
+/* Theme */
+.light-background {
+  background-color: $light-gray;
+}
+.dark-background {
+  background-color: $dark-blue;
+}
+.light-background {
+  background-color: $light-gray;
+}
+
+.dark-background {
+  background-color: $dark-blue;
+}
+
+.light-text {
+  color: $white;
+}
+
+.dark-text {
+  color: $black;
+}
+
+.light-field {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+.dark-field {
+  background: rgba(198, 208, 235, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
+}
+
+.light-link {
+  color: rgba(255, 255, 255, 0.3);
+}
+
+.dark-link {
+  color: rgba(0, 0, 0, 0.3);
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/global-styles/color.scss";
+               @import "@/global-styles/typography.scss";
+              `
+      }
+    }
+  }
+};
